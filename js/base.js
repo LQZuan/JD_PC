e6ee6aacdaefc51f858e7278199454898aed891a
linqingzuan <linqingzuan2016@sina.com>
2018-08-20 22:49:45 +0800

    完成京东手机二维码功能开发

--- js/base.js
+++ js/base.js
@@ -2,7 +2,17 @@ window.onload = function () {
     
     var tpb = document.getElementsByClassName('top-banner')[0];
     var close = document.getElementsByClassName('close-banner')[0];
+    var Code = document.getElementsByClassName('erweima')[0];
+    var jdTel = document.getElementsByClassName('jn-tel')[0];
 
+    //手机京东
+    jdTel.onmouseover = function () {
+        Code.className = 'erweima show';
+    }
+
+    jdTel.onmouseout = function () {
+        Code.className = 'erweima hide';
+    }
 
     //关闭广告栏
     close.onclick = function () {
